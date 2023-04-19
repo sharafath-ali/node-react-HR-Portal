d647e03ef555c8f605339ee912e38a2db69d35f1
Sharafath Ali <sharafath.ali@mydock365.com>
2023-04-19 04:22:19 -0700

    ui completed

--- client/src/compoents/EmployeeProfile.tsx
+++ client/src/compoents/EmployeeProfile.tsx
@@ -1,13 +1,43 @@
-
-
-export interface IEmployeeProfileProps {
-
-}
+import { Link, useParams } from "react-router-dom";
+import "./EmployeeProfile.css";
+import { useState } from "react";
+export interface IEmployeeProfileProps {}
 
 export default function EmployeeProfile(props: IEmployeeProfileProps) {
-    return (
-        <div>
-
+  const [data, setdata] = useState<any>();
+  let { id } = useParams();
+  return (
+    <>
+      <div className="styles.section2">
+        <h1>Profile</h1>
+        <Link className="button1" to={`/`}>
+          Home
+        </Link>
+        <Link className="button1" to={`/employeedetail/${Number(id)}`}>
+          Profile
+        </Link>
+        <Link className="button1" to={`/Documents/${Number(id)}`}>
+          Documents
+        </Link>
+      </div>
+      <div className="employcontainer">
+        <div className="image">
+          <img src={`${data?.Image_url}`} alt="Employee" />
         </div>
-    );
+        <h1>
+          Name:{" "}
+          <span className="space">
+            {data?.first_name} {data?.last_name}
+          </span>
+        </h1>
+        <h2>
+          Designation: <span className="space">{data?.designation}</span>
+        </h2>
+        <h2>
+          Email: <span className="space">{data?.email}</span>
+        </h2>
+      </div>
+      <p style={{ textAlign: "center" }}>all copyright reserved</p>
+    </>
+  );
 }
